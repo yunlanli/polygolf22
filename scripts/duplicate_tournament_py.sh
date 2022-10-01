9ff06a48c07646b14c24ed336fcd1436dade5f7f
yunlanli <chnsidney@gmail.com>
2022-09-30 23:29:20 -0400

    generate run_tournament_xxx py files and map json configurations

--- scripts/duplicate_tournament_py.sh
+++ scripts/duplicate_tournament_py.sh
@@ -2,8 +2,7 @@
 
 file_to_copy="tournament_rerun_base.py"
 
-# for map in "g4/n" "g3/baseball" "g4/jump" "g2/mwc_swirly" "g4/chevuoi" "g9/rhino_karma" "g8/risk_map" "g2/tournament" "g1/tournament" "g3/g3_tournament" "g5/tourney" "g7/g7_final" "default/checkers2" "2021_maps/g7/complex2" "2021_maps/g2/spider_par6";
-for map in "g4/n" "g3/baseball";
+for map in "g4/n" "g3/baseball" "g4/jump" "g2/mwc_swirly" "g4/chevuoi" "g9/rhino_karma" "g8/risk_map" "g2/tournament" "g1/tournament" "g3/g3_tournament" "g5/tourney" "g7/g7_final" "default/checkers2" "2021_maps/g7/complex2" "2021_maps/g2/spider_par6";
 do
 	suffix=`echo $map | sed s@/@_@g`
 	map_config_fn="g1_maps_rerun_$suffix.json"
