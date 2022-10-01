473c0448480ba480fc1f78f1e63a0815d84b4856
yunlanli <chnsidney@gmail.com>
2022-09-30 23:26:48 -0400

    update run tournament scripts

--- scripts/run_tournament_maps.sh
+++ scripts/run_tournament_maps.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
-# generate results directory
 base_dir="g1_results"
 
+# generate results directory
 for map in "g4/n" "g3/baseball";
 do
 	suffix=`echo $map | sed s@/@_@g`
-	python_fn="tournament_rerun_$suffix.py"
 
 	echo "create results directory \"$base_dir/$suffix\" for map \"$map\"..."
 	mkdir -p $base_dir/$suffix
@@ -14,5 +13,14 @@ done
 
 seed_entropy="206877182951751619128817595840757235598"
 
-python tournament_rerun_g4_n.py --result_dir $base_dir/g4_n -s $seed_entropy -t 1 -v
-python tournament_rerun_g3_baseball.py --result_dir $base_dir/g3_baseball -s $seed_entropy -t 2 -v
+
+# run group 1 player on each map
+for map in "g4/n" "g3/baseball";
+do
+	suffix=`echo $map | sed s@/@_@g`
+	python_fn="tournament_rerun_$suffix.py"
+
+	echo "running player 1 on map \"$map\" using python file \"$python_fn\"..."
+	python "tournament_rerun_$suffix.py" --result_dir "$base_dir/$suffix" -s $seed_entropy -t 1 -v
+	echo
+done
