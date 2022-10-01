143b0fff8a97287ae0f87b21838dd8b345802649
yunlanli <chnsidney@gmail.com>
2022-09-30 23:35:56 -0400

    update map order in run tournament scripts

--- scripts/run_tournament_maps.sh
+++ scripts/run_tournament_maps.sh
@@ -15,7 +15,21 @@ seed_entropy="206877182951751619128817595840757235598"
 
 
 # run group 1 player on each map
-for map in "g4/n" "g3/baseball";
+for map in	"g4/n"					\
+			"g4/jump"				\
+			"g8/risk_map" 			\
+			"g3/g3_tournament"		\
+			"g3/baseball"			\
+			"g2/mwc_swirly"			\
+			"g4/chevuoi"			\
+			"g9/rhino_karma"		\
+			"g2/tournament"			\
+			"g1/tournament"			\
+			"g5/tourney"			\
+			"g7/g7_final"			\
+			"default/checkers2"		\
+			"2021_maps/g7/complex2"	\
+			"2021_maps/g2/spider_par6";
 do
 	suffix=`echo $map | sed s@/@_@g`
 	python_fn="tournament_rerun_$suffix.py"
